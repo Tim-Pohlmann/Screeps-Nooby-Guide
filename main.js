42957f6f10ec47e774d825ab9c02891f2df40dd3
Tim Pohlmann <th_pion@gmx.de>
2016-06-29 22:34:57 +0200

    Part 4 - Spawning Creeps

--- main.js
+++ main.js
@@ -3,6 +3,15 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
+    // check for memory entries of died creeps by iterating over Memory.creeps
+    for (let name in Memory.creeps) {
+        // and checking if the creep is still alive
+        if (Game.creeps[name] == undefined) {
+            // if not, delete the memory entry
+            delete Memory.creeps[name];
+        }
+    }
+
     // for every creep name in Game.creeps
     for (let name in Game.creeps) {
         // get the creep object
@@ -16,5 +25,32 @@ module.exports.loop = function () {
         else if (creep.memory.role == 'upgrader') {
             roleUpgrader.run(creep);
         }
+    }
+
+    // goal: have 10 harvesters and as many upgraders as possible
+    var minimumNumberOfHarvesters = 10;
+    // _.sum will count the number of properties in Game.creeps filtered by the
+    //  arrow function, which checks for the creep being a harvester
+    var numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
+    var name = undefined;
+
+    // if not enough harvesters
+    if (numberOfHarvesters < minimumNumberOfHarvesters) {
+        // try to spawn one
+        name = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE], undefined,
+            { role: 'harvester', working: false});
+    }
+    else {
+        // else try to spawn an upgrader
+        // small change from what you saw in the video: for upgraders it makes
+        //  more sense to have two move parts because they have to travel further
+        name = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE,MOVE], undefined,
+            { role: 'upgrader', working: false});
+    }
+
+    // print name to console if spawning was a success
+    // name > 0 would not work since string > 0 returns false
+    if (!(name < 0)) {
+        console.log("Spawned new creep: " + name);
     }
 };

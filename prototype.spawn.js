62feb66a06b5e1f88e6b7bf364b2ab3f952536d2
Tim Pohlmann <th_pion@gmx.de>
2016-07-01 00:05:14 +0200

    Part 7 - Advanced Spawning and Prototypes

---
+++ prototype.spawn.js
@@ -0,0 +1,21 @@
+module.exports = function() {
+    // create a new function for StructureSpawn
+    StructureSpawn.prototype.createCustomCreep =
+        function(energy, roleName) {
+            // create a balanced body as big as possible with the given energy
+            var numberOfParts = Math.floor(energy / 200);
+            var body = [];
+            for (let i = 0; i < numberOfParts; i++) {
+                body.push(WORK);
+            }
+            for (let i = 0; i < numberOfParts; i++) {
+                body.push(CARRY);
+            }
+            for (let i = 0; i < numberOfParts; i++) {
+                body.push(MOVE);
+            }
+
+            // create creep with the created body and the given role
+            return this.createCreep(body, undefined, { role: roleName, working: false });
+        };
+};

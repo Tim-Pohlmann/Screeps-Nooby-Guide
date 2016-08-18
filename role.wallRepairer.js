e295239a864d871af9dda50698b87b0008451396
lispeaking <lispeaking@users.noreply.github.com>
2016-08-18 19:21:35 +0200

    Update role.wallRepairer.js

--- role.wallRepairer.js
+++ role.wallRepairer.js
@@ -1,6 +1,5 @@
 var roleBuilder = require('role.builder');
 var roleCollector = require('role.collector');
-var roleUpgrader = require('role.upgrader');
 
 module.exports = {
     // a function to run the logic for this role
@@ -18,7 +17,7 @@ module.exports = {
 
         // if creep is supposed to repair something
         if (creep.memory.working == true) {
-            var constructionSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES, { filter: (s) => (s.structureType == STRUCTURE_WALL && s.structure.hits < 20000) || (s.structureType == STRUCTURE_RAMPART && s.structureType.hits < 20000)});
+            var constructionSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES, { filter: (s) => s.structureType == STRUCTURE_WALL || s.structureType == STRUCTURE_RAMPART});
             if (constructionSite != null) {
                 // Construction sites found
                 if (creep.build(constructionSite) == ERR_NOT_IN_RANGE) {
@@ -27,12 +26,10 @@ module.exports = {
                 }
             }
             else {
-                roleUpgrader.run(creep);
-                /*
                 var target = undefined;
                 // loop with increasing percentages
-                for (var percentage = 0.1; percentage <= 1; percentage = percentage + 0.1) {
-                    target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (s) => (s.structureType == STRUCTURE_WALL || s.structureType == STRUCTURE_RAMPART) && s.hits / s.hitsMax < percentage});
+                for (var percentage = 0.0001; percentage <= 1; percentage = percentage + 0.0001) {
+                    target = creep.pos.findClosestByPath(FIND_STRUCTURES, {filter: (s) => (s.structureType == STRUCTURE_WALL && s.hits / s.hitsMax < percentage) || (s.structureType == STRUCTURE_RAMPART && s.hits / (s.hitsMax * 100) < percentage)});
                     if (target != undefined) {
                         break;
                     }
@@ -50,7 +47,7 @@ module.exports = {
                 else {
                     // look for construction sites
                     roleBuilder.run(creep);
-                }*/
+                }
             }
         }
         // if creep is supposed to harvest energy from source
